45c6291a11fa9e4484cc7d3c5d7c5284faf009df
RoneyCh <roneychristian159@gmail.com>
2022-03-09 16:39:36 -0300

    cria digito + valida CPF

--- index.js
+++ index.js
@@ -9,18 +9,32 @@ function InputCPF(cpf) {
 InputCPF.prototype.valida = function() {
     if(typeof this.cpfLimpo === 'undefined') return false;
     if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequence()) return false;
     
     const cpfParcial = this.cpfLimpo.slice(0,-2);
-    const dig1 =    this.sum(cpfParcial);
-    return true;
+    const dig1 = this.criaDigito(cpfParcial);
+    const dig2 = this.criaDigito(cpfParcial + dig1);
+    const novoCPF = cpfParcial + dig1 + dig2;
+    return novoCPF === this.cpfLimpo;
 }
 
-InputCPF.prototype.sum = function(cpfParcial) {
+InputCPF.prototype.criaDigito = function(cpfParcial) {
     const cpfArray = Array.from(cpfParcial);
-    cpfArray.reduce((ac,val) => ac += Number(val), 0);
+    let regCount = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regCount*Number(val));
+        regCount--;     
+        return ac;
+    },0)
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : digito;
+};
+
+InputCPF.prototype.isSequence = function() {
+    return this.cpfLimpo[0].repeat(this.cpfLimpo.length)
 }
 
-const p = new InputCPF('111.111.111-55');
-console.log(p.cpfLimpo);
-console.log(p.sum());
+
+const p = new InputCPF('111.111.111.11');
+console.log(p.valida());
 
